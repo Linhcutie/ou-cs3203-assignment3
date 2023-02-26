84943c551cb70ebd7cd45654155a638cd46fe50e
Ngo Linh <ngolinh@Ngos-MacBook-Pro.local>
2023-02-26 14:36:23 -0600

    commit 3 which take user input and return sum and product

--- total.java/src/App.java
+++ total.java/src/App.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -5,6 +6,22 @@ public class App {
          int arr[] = { 1, 2, 3, 4, 5 };
         System.out.println(sum1(arr));
         System.out.println(product(arr));
+
+        int n = 0;
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+
+        System.out.println("Enter all the elements:");
+        for (int i = 0; i < n; i++) {
+            a[i] = s.nextInt();
+        }
+
+        System.out.println("Sum = "+sum1(a));
+        System.out.println("Product = "+product(a));
+        s.close();
     }
 
     public static int sum1(int []copy) 
