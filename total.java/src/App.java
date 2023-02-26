0cea504b190f920e849de45245f45e447f59f6f5
Ngo Linh <ngolinh@Ngos-MacBook-Pro.local>
2023-02-26 14:26:07 -0600

    result is 120

--- total.java/src/App.java
+++ total.java/src/App.java
@@ -4,6 +4,7 @@ public class App {
 
          int arr[] = { 1, 2, 3, 4, 5 };
         System.out.println(sum1(arr));
+        System.out.println(product(arr));
     }
 
     public static int sum1(int []copy) 
@@ -16,6 +17,19 @@ public class App {
         }
     
         return sum;
-        //
     }
+
+    public static int product(int []copy) 
+    {
+        int product=1;
+        
+        for(int i=0; i< copy.length;i++)
+        {
+            product= product*copy[i];
+        }
+    
+        return product;
+    }
+
+
 }
